dffb9759ea339cb8ef2eacf7377a12c6f67c7238
shashi <0905shashi@gmail.com>
2020-12-06 22:30:15 +0530

    Initial commit for spring boot rest service for airlines demo project

---
+++ src/main/java/com/shashi/demo/airlinesrest/AirlinesRestApplication.java
@@ -0,0 +1,13 @@
+package com.shashi.demo.airlinesrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlinesRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlinesRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shashi/demo/airlinesrest/AirlinesRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shashi.demo.airlinesrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlinesRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
